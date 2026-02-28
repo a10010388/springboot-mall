dae3e1cc5a82d89d4117f6d51f9e3181a49538c4
daiyuxuan <a10010338@gmail.com>
2026-02-28 14:08:59 +0800

    product feature

---
+++ src/main/java/com/sam/springbootmall/dao/ProductDao.java
@@ -0,0 +1,9 @@
+package com.sam.springbootmall.dao;
+
+import com.sam.springbootmall.model.Product;
+
+public interface ProductDao {
+
+    Product getProductById(Integer productId);
+
+}
---
+++ src/main/java/com/sam/springbootmall/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.sam.springbootmall.service;
+
+import com.sam.springbootmall.model.Product;
+
+public interface ProductService {
+    Product getProductById(Integer id);
+}
