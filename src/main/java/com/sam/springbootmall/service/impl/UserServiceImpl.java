f593edf1b07671e834e16dfcfbf095eccaeefb47
daiyuxuan <a10010338@gmail.com>
2026-03-02 15:48:56 +0800

    checkEmail add

--- src/main/java/com/sam/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/sam/springbootmall/service/impl/UserServiceImpl.java
@@ -4,17 +4,29 @@ import com.sam.springbootmall.dao.UserDao;
 import com.sam.springbootmall.dto.UserRegisterRequest;
 import com.sam.springbootmall.model.User;
 import com.sam.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+        if(user != null){
+            logger.warn("already exist email{}:",user.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         return userDao.createUser(userRegisterRequest);
     }
 
