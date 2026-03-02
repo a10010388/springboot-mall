60fa96e031986d06036660d7c833b3888c7b0e15
daiyuxuan <a10010338@gmail.com>
2026-03-02 15:20:48 +0800

    register add

---
+++ src/main/java/com/sam/springbootmall/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.sam.springbootmall.dao;
+
+import com.sam.springbootmall.dto.UserRegisterRequest;
+import com.sam.springbootmall.model.User;
+
+public interface UserDao {
+    Integer createUser(UserRegisterRequest userRegisterRequest);
+
+    User getUserById(Integer userId);
+}
---
+++ src/main/java/com/sam/springbootmall/dao/impl/UserDaoImpl.java
@@ -0,0 +1,55 @@
+package com.sam.springbootmall.dao.impl;
+
+import com.sam.springbootmall.dao.UserDao;
+import com.sam.springbootmall.dto.UserRegisterRequest;
+import com.sam.springbootmall.model.User;
+import com.sam.springbootmall.rowmaaper.UserRowMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Override
+    public Integer createUser(UserRegisterRequest userRegisterRequest) {
+        String sql = "insert into mall.user (email, password, created_date, last_modified_date)" +
+                "values ( :email, :password, :createdDate, :lastModifiedDate)";
+        Map<String, Object> map = new HashMap<>();
+        map.put("email", userRegisterRequest.getEmail());
+        map.put("password", userRegisterRequest.getPassword());
+        Date date = new Date();
+        map.put("createdDate", date);
+        map.put("lastModifiedDate", date);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource(map),keyHolder);
+        int userId = keyHolder.getKey().intValue();
+        return userId;
+    }
+
+    @Override
+    public User getUserById(Integer userId) {
+        String sql = "select user_id, email, password, created_date, last_modified_date\n" +
+                "from user where user_id = :userId";
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", userId);
+
+        List<User> userList = namedParameterJdbcTemplate.query(sql,map,new UserRowMapper());
+        if(userList.size()>0){
+            return userList.get(0);
+        }else{
+            return null;
+        }
+    }
+}
